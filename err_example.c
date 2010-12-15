643f711971d60a728bd8238c5f7ef13ca587bfb5
Genas2 <Gennas2@gmail.com>
2010-12-15 04:51:05 +0200

    Added exersice 6: strerror and perror functions demonstration

---
+++ err_example.c
@@ -0,0 +1,12 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    fprintf(stderr, "EACCES: %s\n", strerror(EACCES));
+    errno = ENOENT;
+    perror(argv[0]);
+    exit(0);
+}
